b3d495c17a3e775b5ad84912ce5f3f0c91fa9f16
TakSakong <k402900@naver.com>
2026-05-14 21:31:24 +0900

    refactor(be) : token expire minustes 30min -> 8 h

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     # Auth
     SECRET_KEY: str  # 반드시 환경변수로 설정 필요 원래 있던 문자열로 jwt 토큰 생성 가능함.
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 8 # 8시간
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     # Services
